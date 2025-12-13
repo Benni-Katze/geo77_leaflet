1da035cb3dd3890dedf638307b3ae9e312b90954
Benni-Katze <carla-dorothea.schenk@student.uni-tuebingen.de>
2025-12-13 11:29:22 +0100

    13.12.2025 Fertige Karte

---
+++ bodenseehaefen.js
@@ -0,0 +1,20 @@
+var bs_points = {
+"type": "FeatureCollection",
+"name": "bodenseehaefen",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "name": "Konstanz", "x": 1021734.847, "y": 6050414.356, "platz": 120 }, "geometry": { "type": "Point", "coordinates": [ 09.181116, 47.660762 ] } },
+{ "type": "Feature", "properties": { "id": 2, "name": "Kreuzlingen", "x": 1022188.344, "y": 6049252.638, "platz": 425 }, "geometry": { "type": "Point", "coordinates": [ 09.194406, 47.648854 ] } },
+{ "type": "Feature", "properties": { "id": 3, "name": "Romanshorn", "x": 1044213.865, "y": 6034728.957, "platz": 320 }, "geometry": { "type": "Point", "coordinates": [ 09.384657, 47.561900 ] } },
+{ "type": "Feature", "properties": { "id": 4, "name": "Arbon", "x": 1050874.969, "y": 6026147.853, "platz": 650 }, "geometry": { "type": "Point", "coordinates": [ 09.440869, 47.512654 ] } },
+{ "type": "Feature", "properties": { "id": 5, "name": "Rorschach", "x": 1056829.325, "y": 6020596.933, "platz": 80 }, "geometry": { "type": "Point", "coordinates": [ 09.499922, 47.478495 ] } },
+{ "type": "Feature", "properties": { "id": 6, "name": "Bregenz", "x": 1085116.933, "y": 6025089.202, "platz": 390 }, "geometry": { "type": "Point", "coordinates": [ 09.727962, 47.507462 ] } },
+{ "type": "Feature", "properties": { "id": 8, "name": "Lindau", "x": 1077802.086, "y": 6031308.589, "platz": 350 }, "geometry": { "type": "Point", "coordinates": [ 09.683597, 47542952 ] } },
+{ "type": "Feature", "properties": { "id": 9, "name": "Langenargen", "x": 1061907.607, "y": 6039630.552, "platz": 200 }, "geometry": { "type": "Point", "coordinates": [ 09.557388, 47.585652 ] } },
+{ "type": "Feature", "properties": { "id": 10, "name": "Friedrichshafen", "x": 1055708.834, "y": 6048830.061, "platz": 270 }, "geometry": { "type": "Point", "coordinates": [ 09.478493, 47.649094 ] } },
+{ "type": "Feature", "properties": { "id": 11, "name": "Immenstaad", "x": 1042779.755, "y": 6050845.767, "platz": 100 }, "geometry": { "type": "Point", "coordinates": [ 09.378035, 47.664321 ] } },
+{ "type": "Feature", "properties": { "id": 12, "name": "Meersburg", "x": 1032085.031, "y": 6055778.282, "platz": 80 }, "geometry": { "type": "Point", "coordinates": [ 09.272340, 47.691520 ] } },
+{ "type": "Feature", "properties": { "id": 13, "name": "Überlingen", "x": 1021907.853, "y": 6066562.086, "platz": 350 }, "geometry": { "type": "Point", "coordinates": [ 09.179908, 47.757376 ] } },
+{ "type": "Feature", "properties": { "id": 14, "name": "Radolfzell", "x": 998435.706, "y": 6062755.951, "platz": 160 }, "geometry": { "type": "Point", "coordinates": [ 08.969792, 47.733937 ] } }
+]
+}
---
+++ bodenseehaefen_backup_2.js
@@ -0,0 +1,20 @@
+var bs_points = {
+"type": "FeatureCollection",
+"name": "bodenseehaefen",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "name": "Konstanz", "x": 1021734.847, "y": 6050414.356, "platz": 120 }, "geometry": { "type": "Point", "coordinates": [ 1021734.846625767298974, 6050414.355828220024705 ] } },
+{ "type": "Feature", "properties": { "id": 2, "name": "Kreuzlingen", "x": 1022188.344, "y": 6049252.638, "platz": 425 }, "geometry": { "type": "Point", "coordinates": [ 1022188.343558282824233, 6049252.638036810792983 ] } },
+{ "type": "Feature", "properties": { "id": 3, "name": "Romanshorn", "x": 1044213.865, "y": 6034728.957, "platz": 320 }, "geometry": { "type": "Point", "coordinates": [ 1044213.865030675311573, 6034728.957055217586458 ] } },
+{ "type": "Feature", "properties": { "id": 4, "name": "Arbon", "x": 1050874.969, "y": 6026147.853, "platz": 650 }, "geometry": { "type": "Point", "coordinates": [ 1050874.969325154554099, 6026147.852760740555823 ] } },
+{ "type": "Feature", "properties": { "id": 5, "name": "Rorschach", "x": 1056829.325, "y": 6020596.933, "platz": 80 }, "geometry": { "type": "Point", "coordinates": [ 1056829.325153375742957, 6020596.932515342719853 ] } },
+{ "type": "Feature", "properties": { "id": 6, "name": "Bregenz", "x": 1085116.933, "y": 6025089.202, "platz": 390 }, "geometry": { "type": "Point", "coordinates": [ 1085116.932515338761732, 6025089.202453994192183 ] } },
+{ "type": "Feature", "properties": { "id": 8, "name": "Lindau", "x": 1077802.086, "y": 6031308.589, "platz": 350 }, "geometry": { "type": "Point", "coordinates": [ 1077802.085889571579173, 6031308.588957059197128 ] } },
+{ "type": "Feature", "properties": { "id": 9, "name": "Langenargen", "x": 1061907.607, "y": 6039630.552, "platz": 200 }, "geometry": { "type": "Point", "coordinates": [ 1061907.607361964648589, 6039630.552147244103253 ] } },
+{ "type": "Feature", "properties": { "id": 10, "name": "Friedrichshafen", "x": 1055708.834, "y": 6048830.061, "platz": 270 }, "geometry": { "type": "Point", "coordinates": [ 1055708.834355829749256, 6048830.061349696479738 ] } },
+{ "type": "Feature", "properties": { "id": 11, "name": "Immenstaad", "x": 1042779.755, "y": 6050845.767, "platz": 100 }, "geometry": { "type": "Point", "coordinates": [ 1042779.754601228283718, 6050845.766871167346835 ] } },
+{ "type": "Feature", "properties": { "id": 12, "name": "Meersburg", "x": 1032085.031, "y": 6055778.282, "platz": 80 }, "geometry": { "type": "Point", "coordinates": [ 1032085.030674848356284, 6055778.2822085916996 ] } },
+{ "type": "Feature", "properties": { "id": 13, "name": "Überlingen", "x": 1021907.853, "y": 6066562.086, "platz": 350 }, "geometry": { "type": "Point", "coordinates": [ 1021907.852760738576762, 6066562.085889575071633 ] } },
+{ "type": "Feature", "properties": { "id": 14, "name": "Radolfzell", "x": 998435.706, "y": 6062755.951, "platz": 160 }, "geometry": { "type": "Point", "coordinates": [ 998435.705521474708803, 6062755.9509202465415 ] } }
+]
+}
